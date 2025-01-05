d54373f9feccbea083610fb3366d3e93c663ca98
Jeton Korenica <jetonkorenica@gmail.com>
2025-01-06 00:31:40 +0100

    Chapter 1 - Building Our MVP

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    frameborder="0"
+    title="Video player"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
