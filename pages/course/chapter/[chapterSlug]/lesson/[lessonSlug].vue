5562a6d6d2be60e73ffc7e74f1eed0cbcd92637e
Jeton Korenica <jetonkorenica@gmail.com>
2025-01-08 00:30:51 +0100

    Making our app robust

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -24,7 +24,7 @@
     <p>{{ lesson.text }}</p>
     <LessonCompleteButton
       :model-value="isLessonCompleted"
-      @update:model-value="toggleLessonCompletion"
+      @update:model-value="toggleLessonCompletion()"
     />
   </div>
 </template>
@@ -32,6 +32,36 @@
 <script setup>
 const course = useCourse();
 const route = useRoute();
+
+definePageMeta({
+  validate({ params }) {
+    const course = useCourse();
+
+    const chapter = course.chapters.find(
+      (chapter) => chapter.slug === params.chapterSlug
+    );
+
+    if (!chapter) {
+      return createError({
+        statusCode: 404,
+        message: 'Chapter not found',
+      });
+    }
+
+    const lesson = chapter.lessons.find(
+      (lesson) => lesson.slug === params.lessonSlug
+    );
+
+    if (!lesson) {
+      return createError({
+        statusCode: 404,
+        message: 'Lesson not found',
+      });
+    }
+
+    return true;
+  },
+});
 
 const chapter = computed(() => {
   return course.chapters.find(
